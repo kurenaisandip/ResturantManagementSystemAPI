9132921d5e5fb9857af11de3de03c47c40a55be1
Sandip <srai68141@gmail.com>
2024-11-16 12:45:10 +0545

    first

---
+++ server.js
@@ -0,0 +1,12 @@
+﻿import express from "express";
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

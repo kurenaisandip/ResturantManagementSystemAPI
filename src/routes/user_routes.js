cd82c73d1239027242f91301e46209e925118b6a
Sandip <srai68141@gmail.com>
2024-11-21 21:55:10 +0545

    feat: added bycrpt js to hash the password before saving in the database

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,31 +1,41 @@
 import express from "express";
 import {User} from "../models/users.js";
+import bycypt from "bcrypt";
 // import {ConnectionStates} from "mongoose";
 
 
 const UserRouter = express.Router();
 
 
-UserRouter.post("/signup", async (req, res,next) => {
-const {name, email, phone_number, password} = req.body;
+UserRouter.post("/signup", async (req, res, next) => {
+    const {name, email, phone_number, password} = req.body;
 
 // validate request -> express validator
-if (!name || !email || !phone_number || !password) {
-    return res.status(400).json({message: "All fields are required"});
-}
+    if (!name || !email || !phone_number || !password) {
+        return res.status(400).json({message: "All fields are required"});
+    }
 
-console.log(name, email, phone_number, password);
+    const salt = await bycypt.genSalt(10);
+    const hashedPassword = await bycypt.hash(password, salt);
 
-const result = await User.create({
-    name,
-    email,
-    phone_number,
-    password,
+    console.log(name, email, phone_number, password);
+
+    const result = await User.create({
+        name,
+        email,
+        phone_number,
+        password : hashedPassword,
+    });
+
+    console.log(result);
+
+    res.status(201).json({id: result._id});
+
 });
 
-console.log(result);
+UserRouter.post("/login", async (req, res, next) => {
 
-res.status(201).json({id: result._id});
+
 
 });
 export default UserRouter;
